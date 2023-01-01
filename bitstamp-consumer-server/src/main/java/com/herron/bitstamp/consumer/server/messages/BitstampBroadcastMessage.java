f8232b903f842911f14f305f4e1843cbd5f57c02
christopherrons <christopherheron09@gmail.com>
2023-01-01 16:22:43 +0100

    bugfix

--- bitstamp-consumer-server/src/main/java/com/herron/bitstamp/consumer/server/messages/BitstampBroadcastMessage.java
+++ bitstamp-consumer-server/src/main/java/com/herron/bitstamp/consumer/server/messages/BitstampBroadcastMessage.java
@@ -3,8 +3,8 @@ package com.herron.bitstamp.consumer.server.messages;
 import com.herron.bitstamp.consumer.server.api.BitstampMarketEvent;
 import com.herron.bitstamp.consumer.server.enums.EventTypeEnum;
 
-public record BitstampBroadcastMessage(BitstampMarketEvent serializedMessage,
-                                       String serializedMessageTypeString,
+public record BitstampBroadcastMessage(BitstampMarketEvent message,
+                                       String messageMessageType,
                                        long sequenceNumber,
                                        long timeStampInMs) implements BitstampMarketEvent {
 
